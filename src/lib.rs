a659c325009cb2f3b63cde13fe7241b231b41ae2
frogtd <ash@ash.lgbt>
2022-07-24 20:11:10 -0400

    into_stored method, iterator traits, and bump version

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,18 @@
 //! ```
 //! use chunk_iter::ChunkIter;
 //!
-//! let iter = vec![0,1,2,3,4,5, 6].into_iter();
+//! let iter = vec![0, 1, 2, 3, 4, 5, 6].into_iter();
 //! let mut chunks = iter.chunks::<3>();
-//! assert_eq!(chunks.next(), Some([0,1,2]));
-//! assert_eq!(chunks.next(), Some([3,4,5]));
+//! assert_eq!(chunks.next(), Some([0, 1, 2]));
+//! assert_eq!(chunks.next(), Some([3, 4, 5]));
 //! assert_eq!(chunks.next(), None);
 //! ```
 #![no_std]
-use core::mem::MaybeUninit;
+use core::{
+    iter::FusedIterator,
+    mem::{ManuallyDrop, MaybeUninit},
+    ptr,
+};
 /// ChunkIter trait: `use` this to use the `chunks` impl.
 pub trait ChunkIter<T, I: Iterator<Item = T>> {
     /// Make chunks:
@@ -18,8 +22,8 @@ pub trait ChunkIter<T, I: Iterator<Item = T>> {
     ///
     /// let iter = vec![0, 1, 2, 3, 4, 5, 6].into_iter();
     /// let mut chunks = iter.chunks::<3>();
-    /// assert_eq!(chunks.next(), Some([0,1,2]));
-    /// assert_eq!(chunks.next(), Some([3,4,5]));
+    /// assert_eq!(chunks.next(), Some([0, 1, 2]));
+    /// assert_eq!(chunks.next(), Some([3, 4, 5]));
     /// assert_eq!(chunks.next(), None);
     /// ```
     fn chunks<const N: usize>(self) -> Chunks<T, I, N>;
@@ -45,12 +49,41 @@ pub struct Chunks<T, I: Iterator<Item = T>, const N: usize> {
 }
 
 impl<T, I: Iterator<Item = T>, const N: usize> Chunks<T, I, N> {
+    const NONE: Option<T> = None;
     /// Gets the number of currently stored things in the backing array.
-    /// This is usally empty, and only will have values after the backing iterator runs out.
+    /// This is usually empty, and only will have values after the backing iterator runs out.
+    /// ```
+    /// use chunk_iter::ChunkIter;
+    ///
+    /// let mut iter = vec![0, 1, 2, 3, 4].into_iter().chunks();
+    /// assert_eq!(iter.next(), Some([0, 1, 2]));
+    /// assert_eq!(iter.next(), None);
+    ///
+    /// assert_eq!(iter.currently_stored(), &[3, 4]);
     pub fn currently_stored(&self) -> &[T] {
         // SAFETY:
         // needs_dropping is the number of elements that are stored in the buffer
         unsafe { &*(&self.buffer[..self.needs_dropping] as *const [_] as *const [T]) }
+    }
+
+    /// Convert into array of currently stored items.
+    /// This will only have values when the backing iterator has run out of values.
+    /// ```
+    /// use chunk_iter::ChunkIter;
+    ///
+    /// let mut iter = vec![0, 1, 2, 3, 4].into_iter().chunks();
+    /// assert_eq!(iter.next(), Some([0, 1, 2]));
+    /// assert_eq!(iter.next(), None);
+    ///
+    /// assert_eq!(iter.into_stored(), [Some(3), Some(4), None]);
+    pub fn into_stored(self) -> [Option<T>; N] {
+        let mut this = ManuallyDrop::new(self);
+        let mut stored = [Self::NONE; N];
+        for (x, item) in stored.iter_mut().enumerate().take(this.needs_dropping) {
+            *item = unsafe { Some(this.buffer[x].as_ptr().read()) };
+        }
+        unsafe { ptr::drop_in_place(&mut this.iterator) };
+        stored
     }
 }
 
@@ -76,6 +109,14 @@ impl<T, I: Iterator<Item = T>, const N: usize> Iterator for Chunks<T, I, N> {
         (lower / N, upper.map(|x| x / N))
     }
 }
+impl<T, I: Iterator<Item = T> + ExactSizeIterator, const N: usize> ExactSizeIterator
+    for Chunks<T, I, N>
+{
+    fn len(&self) -> usize {
+        self.iterator.len() / N
+    }
+}
+impl<T, I: Iterator<Item = T> + FusedIterator, const N: usize> FusedIterator for Chunks<T, I, N> {}
 
 impl<T, I: Iterator<Item = T>, const N: usize> Drop for Chunks<T, I, N> {
     fn drop(&mut self) {
@@ -114,7 +155,6 @@ mod tests {
         drop(chunks);
 
         assert_eq!(10, test_drop.num_tracked_items());
-        assert_eq!(10, test_drop.num_dropped_items());
     }
 
     #[test]
@@ -124,5 +164,15 @@ mod tests {
             .chunks::<3>();
 
         assert_eq!(iter.size_hint(), (2, Some(2)))
+    }
+
+    #[test]
+    fn currently_stored_test() {
+        let mut iter = alloc::vec![0, 1, 2, 3, 4].into_iter().chunks::<3>();
+        iter.next();
+        assert!(iter.next().is_none());
+        assert!(iter.next().is_none());
+        assert_eq!(iter.currently_stored(), &[3, 4]);
+        assert_eq!(iter.into_stored(), [Some(3), Some(4), None]);
     }
 }
